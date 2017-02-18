c1c80a7f9d3b7c83a60b6cc510757730a4de0a63
huyenha <hathehuyen@gmail.com>
2017-02-18 11:53:24 +0700

    Change conflict variable in get_disk

--- dms.py
+++ dms.py
@@ -32,21 +32,21 @@ def get_disk():
     root_dir_pattern = re.compile('^.*?/devices')
     internal_devices = []
 
-    def device_state(name):
+    def device_state(dev_name):
         with open('/sys/block/%s/device/block/%s/removable' % (name, name)) as f:
             if f.read(1) == '1':
                 return
-        path = root_dir_pattern.sub('', os.readlink('/sys/block/%s' % name))
+        dev_path = root_dir_pattern.sub('', os.readlink('/sys/block/%s' % name))
         hot_plug_buses = ("usb", "ieee1394", "mmc", "pcmcia", "firewire")
         for bus in hot_plug_buses:
             if os.path.exists('/sys/bus/%s' % bus):
                 for device_bus in os.listdir('/sys/bus/%s/devices' % bus):
                     device_link = root_dir_pattern.sub('', os.readlink(
                         '/sys/bus/%s/devices/%s' % (bus, device_bus)))
-                    if re.search(device_link, path):
+                    if re.search(device_link, dev_path):
                         return
 
-        internal_devices.append(name)
+        internal_devices.append(dev_name)
 
     for path in glob('/sys/block/*/device'):
         name = re.sub('.*/(.*?)/device', '\g<1>', path)
