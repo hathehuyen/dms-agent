975ca537f732a4b179d012fe76597b327808193d
huyenha <hathehuyen@gmail.com>
2017-02-18 13:34:04 +0700

    Add download function

--- dms.py
+++ dms.py
@@ -1,23 +1,48 @@
 #!/usr/bin/python
-import fcntl
 import os
-import struct
 import sys
 import time
 import psutil
 import json
 import re
+from urllib2 import urlopen
 from glob import glob
 from datetime import datetime
 from os.path import getmtime
 
+version = 1
+update_url = "http://vnexpress.net"
 # Check modified time at startup
 WATCHED_FILES = [__file__]
 WATCHED_FILES_MTIMES = [(f, getmtime(f)) for f in WATCHED_FILES]
 
 
+def download(url):
+    file_name = url.split('/')[-1]
+    u = urlopen(url)
+    f = open(file_name, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        status = status + chr(8) * (len(status) + 1)
+        print status,
+
+    f.close()
+
 def check_for_update():
-    pass
+    download(update_url)
+
 
 
 def restart_if_modified():
@@ -51,18 +76,18 @@ def get_disk():
     for path in glob('/sys/block/*/device'):
         name = re.sub('.*/(.*?)/device', '\g<1>', path)
         device_state(name)
-    print(' '.join(internal_devices))
+    # print(' '.join(internal_devices))
     return internal_devices
 
 
 def hddinfo(hddev):
-    pass
+    print os.stat(hddev).st_size
+    # pass
 
 while True:
     check_for_update()
     restart_if_modified()
-    get_disk()
-    # hddinfo('/dev/sda')
+    print get_disk()
+    hddinfo('/dev/sda')
     time.sleep(1)
-
     print datetime.now()
