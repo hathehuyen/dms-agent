76e0aa6bf9b481126d27676f6111ff0913b8ab0c
huyenha <hathehuyen@gmail.com>
2017-02-18 09:42:47 +0700

    Add function restart_if_modified

--- dms.py
+++ dms.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python
+import os
+import sys
+import time
+from os.path import getmtime
+from datetime import datetime
+
+# Check modified time at startup
+WATCHED_FILES = [__file__]
+WATCHED_FILES_MTIMES = [(f, getmtime(f)) for f in WATCHED_FILES]
+
+
+def check_for_update():
+    pass
+
+
+def restart_if_modified():
+    # Check whether a watched file has modified.
+    for f, mtime in WATCHED_FILES_MTIMES:
+        if getmtime(f) != mtime:
+            print('File modified, restarting')
+            os.execv(__file__, sys.argv)
+
+while True:
+    check_for_update()
+    restart_if_modified()
+    time.sleep(1)
+    print datetime.now()
